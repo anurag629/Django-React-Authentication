e04981f444a8acf691d10ccefa048d4392ab8160
Your Name <you@example.com>
2022-02-19 23:21:35 +0530

    created auth.js reducer and actions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,21 +8,27 @@ import ResetPassword from './containers/ResetPassword';
 import ResetPasswordConfirm from './containers/ResetPasswordConfirm';
 import Signup from './containers/Signup';
 
+import { Provider } from 'react-redux';
+import store from './store';
+
 import Layout from './hocs/Layout';
 
 const App = () => (
-  <Router>
-    <Layout>
-      <Routes>
-        <Route exact path='/' element={<Home />} />
-        <Route exact path='/login' element={<Login />} />
-        <Route exact path='/activate/:uid/:token' element={<Activate />} />
-        <Route exact path='/reset-password' element={<ResetPassword />} />
-        <Route exact path='/password/reset/confirm/:uid/:token' element={<ResetPasswordConfirm />} />
-        <Route exact path='/signup' element={<Signup />} />
-      </Routes>
-    </Layout>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/login' element={<Login />} />
+          <Route exact path='/activate/:uid/:token' element={<Activate />} />
+          <Route exact path='/reset-password' element={<ResetPassword />} />
+          <Route exact path='/password/reset/confirm/:uid/:token' element={<ResetPasswordConfirm />} />
+          <Route exact path='/signup' element={<Signup />} />
+        </Routes>
+      </Layout>
+    </Router>
+  </Provider>
+
 );
 
 export default App;
