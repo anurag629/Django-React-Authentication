f678a24d2833d76f20195dd7546b088868044384
ANURAG VERMA <20cse08@ietagra.org.in>
2022-03-01 20:11:29 +0530

    all done

--- frontend/src/actions/auth.js
+++ frontend/src/actions/auth.js
@@ -9,7 +9,11 @@ import {
     PASSWORD_RESET_SUCCESS,
     PASSWORD_RESET_CONFIRM_FAIL,
     PASSWORD_RESET_CONFIRM_SUCCESS,
-    PASSWORD_RESET_FAIL
+    PASSWORD_RESET_FAIL,
+    SIGNUP_SUCCESS,
+    SIGNUP_FAIL,
+    ACTIVATION_SUCCESS,
+    ACTIVATION_FAIL
 } from "./types";
 
 import axios from 'axios';
@@ -110,6 +114,53 @@ export const login = (email, password) => async dispatch => {
     }
 };
 
+export const signup = (name, email, password, re_password) => async dispatch => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    const body = JSON.stringify({ name, email, password, re_password });
+
+    try {
+        const res = await axios.post(`${process.env.REACT_APP_API_URL}/auth/user/`, body, config);
+
+        dispatch({
+            type: SIGNUP_SUCCESS,
+            payload: res.data
+        });
+
+    } catch (err) {
+        dispatch({
+            type: SIGNUP_FAIL
+        });
+    }
+};
+
+export const verify = (uid, token) => async dispatch => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    const body = JSON.stringify({ uid, token });
+
+    try {
+        await axios.post(`${process.env.REACT_APP_API_URL}/auth/users/activation/`, body, config);
+
+        dispatch({
+            type: ACTIVATION_SUCCESS,
+        });
+
+    } catch (err) {
+        dispatch({
+            type: ACTIVATION_FAIL
+        });
+    }
+};
+
 export const reset_password = (email) => async dispatch => {
     const config = {
         headers: {
@@ -120,7 +171,7 @@ export const reset_password = (email) => async dispatch => {
     const body = JSON.stringify({ email });
 
     try {
-        await axios.post(`${process.env.REACT_APP_API_URL}/auth/user/reset_password/`, body, config);
+        await axios.post(`${process.env.REACT_APP_API_URL}/auth/users/reset_password/`, body, config);
 
         dispatch({
             type: PASSWORD_RESET_SUCCESS
@@ -143,7 +194,7 @@ export const reset_password_confirm = (uid, token, new_password, re_new_password
     const body = JSON.stringify({ uid, token, new_password, re_new_password });
 
     try {
-        await axios.post(`${process.env.REACT_APP_API_URL}/auth/user/reset_password_comfirm/`, body, config);
+        await axios.post(`${process.env.REACT_APP_API_URL}/auth/users/reset_password_confirm/`, body, config);
 
         dispatch({
             type: PASSWORD_RESET_CONFIRM_SUCCESS
